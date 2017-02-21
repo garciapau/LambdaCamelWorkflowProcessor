f3de362e80d2ad102fab2db831c618589f8b80db
Pau García <pau.garciazorrilla@thomsonreuters.com>
2017-02-21 22:01:07 +0100

    v1.0 routing based on wfId, taskId and action

--- src/content/integration/processor/AWSDDBEventProcessorBean.java
+++ src/content/integration/processor/AWSDDBEventProcessorBean.java
@@ -10,4 +10,26 @@ public class AWSDDBEventProcessorBean {
 
         return ddbEvent.getRecords().get(0).getDynamodb().getKeys().get("workflowId").getS();
     }
+
+    public static String getTaskId(DynamodbEvent ddbEvent) {
+        System.out.println("\n\t\t ProcessorBean retrieves " + ddbEvent.getRecords().size() + " events");
+        System.out.println("\n\t\t ProcessorBean retrieves taskId: " + ddbEvent.getRecords().get(0).getDynamodb().getKeys().get("taskId").getS());
+
+        return ddbEvent.getRecords().get(0).getDynamodb().getKeys().get("taskId").getS();
+    }
+
+    public static String getAction(DynamodbEvent ddbEvent) {
+        System.out.println("\n\t\t ProcessorBean retrieves " + ddbEvent.getRecords().size() + " events");
+        System.out.println("\n\t\t ProcessorBean retrieves taskId: " + ddbEvent.getRecords().get(0).getDynamodb().getNewImage().get("action").getS());
+
+        return ddbEvent.getRecords().get(0).getDynamodb().getNewImage().get("action").getS();
+    }
+
+    public static String getUser(DynamodbEvent ddbEvent) {
+        System.out.println("\n\t\t ProcessorBean retrieves " + ddbEvent.getRecords().size() + " events");
+        System.out.println("\n\t\t ProcessorBean retrieves taskId: " + ddbEvent.getRecords().get(0).getDynamodb().getNewImage().get("userId").getS());
+
+        return ddbEvent.getRecords().get(0).getDynamodb().getNewImage().get("userId").getS();
+    }
+
 }
